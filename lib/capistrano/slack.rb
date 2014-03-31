ed3f760f4af2929060bed8c1a2fbf010bbb72961
Nicholas Van Wiggeren <nick@facepwn.com>
2014-03-31 16:41:24 -0700

    Remove erroneous try/catch block

--- lib/capistrano/slack.rb
+++ lib/capistrano/slack.rb
@@ -54,38 +54,32 @@ module Capistrano
 
 
             task :finished do
-              begin
-                slack_token = fetch(:slack_token)
-                slack_room = fetch(:slack_room)
-                slack_emoji = fetch(:slack_emoji) || ":ghost:"
-		        slack_username = fetch(:slack_username) || "deploybot"
-		        slack_application = fetch(:slack_application) || application
-                slack_subdomain = fetch(:slack_subdomain)
-                return if slack_token.nil?
-                announced_deployer = fetch(:deployer)
-                end_time = Time.now
-                start_time = fetch(:start_time)
-                elapsed = end_time.to_i - start_time.to_i
+              slack_token = fetch(:slack_token)
+              slack_room = fetch(:slack_room)
+              slack_emoji = fetch(:slack_emoji) || ":ghost:"
+	            slack_username = fetch(:slack_username) || "deploybot"
+	            slack_application = fetch(:slack_application) || application
+              slack_subdomain = fetch(:slack_subdomain)
+              return if slack_token.nil?
+              announced_deployer = fetch(:deployer)
+              end_time = Time.now
+              start_time = fetch(:start_time)
+              elapsed = end_time.to_i - start_time.to_i
+            
+              msg = "#{announced_deployer} deployed #{slack_application} successfully in #{elapsed} seconds."
               
-                msg = "#{announced_deployer} deployed #{slack_application} successfully in #{elapsed} seconds."
-                
-                # Parse the URI and handle the https connection
-                uri = URI.parse("https://#{slack_subdomain}.slack.com/services/hooks/incoming-webhook?token=#{slack_token}")
-                http = Net::HTTP.new(uri.host, uri.port)
-                http.use_ssl = true
-                http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+              # Parse the URI and handle the https connection
+              uri = URI.parse("https://#{slack_subdomain}.slack.com/services/hooks/incoming-webhook?token=#{slack_token}")
+              http = Net::HTTP.new(uri.host, uri.port)
+              http.use_ssl = true
+              http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-                # Create the post request and setup the form data
-                request = Net::HTTP::Post.new(uri.request_uri)
-                request.set_form_data(:payload => {'channel' => slack_room, 'username' => slack_username, 'text' => msg, "icon_emoji" => slack_emoji}.to_json)
-                
-                # Make the actual request to the API
-                response = http.request(request)
-
-              rescue Faraday::Error::ParsingError
-                # FIXME deal with crazy color output instead of rescuing
-                # it's stuff like: ^[[0;33m and ^[[0m
-              end
+              # Create the post request and setup the form data
+              request = Net::HTTP::Post.new(uri.request_uri)
+              request.set_form_data(:payload => {'channel' => slack_room, 'username' => slack_username, 'text' => msg, "icon_emoji" => slack_emoji}.to_json)
+              
+              # Make the actual request to the API
+              response = http.request(request)
             end
           end
         end
